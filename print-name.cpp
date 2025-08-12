47be0bec9585a22385fe23f77b4be69154431a0f
malavika-vinu <vpillai.malavika@gmail.com>
2025-08-12 21:25:01 +0530

    print-values
    Displaying

---
+++ print-name.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+int main(){
+ std::string name;
+ 
+ std::cout<<"Enter your name.";
+ 
+ std::cin>>name;
+ 
+ std::cout<<"Namah Shivaya,"<<name<<"!"<<std::endl;
+ 
+ 
+ return 0;
+ 
+}
