97966f7659aad8395a740b127d6140c570f409b7
malavika-vinu <vpillai.malavika@gmail.com>
2025-08-14 23:22:49 +0530

    Add files via upload

---
+++ greetings.cpp
@@ -0,0 +1,11 @@
+//Program to execute greetings.
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+ cout << "Namah Shivaya" << endl; //Displays greeting
+ 
+return 0;                         //Indicates successful program execution
+
+}
---
+++ greetings_with_name.cpp
@@ -0,0 +1,18 @@
+//Program to output greetings with name.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+ string name;       //declare variable to store name.
+ 
+ cout << "Enter your name.";
+ 
+ cin >> name;      //stores input from user to 'name' variable.
+ 
+ cout << "Namah Shivaya," << name << "!" << endl;
+ 
+ 
+ return 0;
+ 
+}
